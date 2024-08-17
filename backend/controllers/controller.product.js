8da8eaab1efbdd0b66eea07793d7381fe51c8a25
Justice Philemon <imailasjay>
2024-08-17 22:35:31 +0100

    some edit on the backed code.

--- backend/controllers/controller.product.js
+++ backend/controllers/controller.product.js
@@ -16,7 +16,7 @@ const createProduct = async (req, res) => {
 const fetchProducts = async (req, res) => {
   try {
     const products = await Product.find();
-    res.status(200).send({ products });
+    res.status(200).send(products);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
